ffd386381b5db7cf81d2483fc96d6b364e060299
WillZealot <127908016+WillZealot@users.noreply.github.com>
2023-05-16 18:26:51 -0400

    changed my code up just a little more so that its easier to understand

--- Develop/script.js
+++ Develop/script.js
@@ -17,22 +17,30 @@ $(function () {
       localStorage.setItem(key, value);
     });
   }
-  userInput();
+
+
   // TODO: Add code to apply the past, present, or future class to each time
   // block by comparing the id to the current hour. HINTS: How can the id
   // attribute of each time-block be used to conditionally add or remove the
   // past, present, and future classes? How can Day.js be used to get the
   // current hour in 24-hour time?
+
+  
   // TODO: Add code to get any user input that was saved in localStorage and set
   // the values of the corresponding textarea elements. HINT: How can the id
   // attribute of each time-block be used to do this?
   //
   // TODO: Add code to display the current date in the header of the page.
    ////////////////////// Date At The Top Code Area//////////////////////////////
-   let todayEl = $('#currentDay'); //Im setting the variable to the id that contains the text area
+   function setDate(){
+    let todayEl = $('#currentDay'); //Im setting the variable to the id that contains the text area
    let d = new Date(); //getting a new date here which is the current one
    let thisDate = dayjs(d).format('dddd [The] DD[th] , MMM YYYY'); //formatting the date to be text
 
-   todayEl.text(thisDate);// setting the text of the text area to my formatted date
+   todayEl.text(thisDate);
+   }
+
+   setDate();
+   userInput();
    ////////////////////////////////////////////////////////////////////////
 });
