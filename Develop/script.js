702b3616def0673ae32bc3f3fd469f8bbfbc9947
WillZealot <127908016+WillZealot@users.noreply.github.com>
2023-05-18 21:38:04 -0400

    finished

--- Develop/script.js
+++ Develop/script.js
@@ -1,52 +1,53 @@
-// Wrap all code that interacts with the DOM in a call to jQuery to ensure that
-// the code isn't run until the browser has finished rendering all the elements
-// in the html.
+// 1. Setting Locale Settings for Day.js
 let localeSettingsEl = {};
   dayjs.locale(localeSettingsEl);
 
-  $(document).ready(function() {// your code that interacts with the DOM goes here
+// 2. Document Ready Event Handler  
+  $(document).ready(function() {
+    $(function () {
 
-});
+// 3. Handling User Input      
+      function userInput() {
+        $('.saveBtn').on('click', function() {
+          let key = $(this).parent().attr('id');
+          let value = $(this).siblings('.description').val();
+          localStorage.setItem(key, value);
+        });
+      }
 
-$(function () {
+// 4. Setting Current Time      
+      let currentTimeEl = dayjs().format('H');
+      console.log(currentTimeEl);
 
-  function userInput() {
-    $('.saveBtn').on('click', function() {
-      let key = $(this).parent().attr('id');
-      let value = $(this).siblings('.description').val();
-      localStorage.setItem(key, value);
+// 5. Setting Colors for Time Blocks      
+      function colourSet() {
+        $('.time-block').each(function() {
+          let hourEl = parseInt(this.id);
+          $(this).toggleClass('past', hourEl < currentTimeEl);
+          $(this).toggleClass('present', hourEl === currentTimeEl);
+          $(this).toggleClass('future', hourEl > currentTimeEl);
+        });
+      }
+
+// 6. Retrieving and Setting Saved User Input      
+      $('.time-block').each(function() {
+        let key = $(this).attr('id');
+        let value = localStorage.getItem(key);
+        $(this).children('.description').val(value);
+      });
+
+// 7. Setting the Current Date      
+       function setDate(){
+        let todayEl = $('#currentDay'); //Im setting the variable to the id that contains the text area
+       let d = new Date(); //getting a new date here which is the current one
+       let thisDate = dayjs(d).format('dddd [The] DD[th] , MMM YYYY'); //formatting the date to be text
+
+//8. Setting The Current Date Into The Top Header       
+       todayEl.text(thisDate);
+       }
+      
+      setDate();
+      colourSet();
+      userInput();
     });
-  }
-
-  let currentTimeEl = dayjs().format('H');
-  console.log(currentTimeEl);
-
-  function colourSet() {
-    $('.time-block').each(function() { //.each instead of a for loop grabbing each timeblock class
-      let hourEl = parseInt(this.id);
-      $(this).toggleClass('past', hourEl < currentTimeEl);
-      $(this).toggleClass('present', hourEl === currentTimeEl);
-      $(this).toggleClass('future', hourEl > currentTimeEl);
-    });
-  }
-  // TODO: Add code to get any user input that was saved in localStorage and set
-  // the values of the corresponding textarea elements. HINT: How can the id
-  // attribute of each time-block be used to do this?
-  $('.time-block').each(function() {
-    let key = $(this).attr('id');
-    let value = localStorage.getItem(key);
-    $(this).children('.description').val(value);
-  });
-   ////////////////////// Date At The Top Code Area//////////////////////////////
-   function setDate(){
-    let todayEl = $('#currentDay'); //Im setting the variable to the id that contains the text area
-   let d = new Date(); //getting a new date here which is the current one
-   let thisDate = dayjs(d).format('dddd [The] DD[th] , MMM YYYY'); //formatting the date to be text
-
-   todayEl.text(thisDate);
-   }
-   ////////////////////////////////////////////////////////////////////////
-  setDate();
-  colourSet();
-  userInput();
 });
--- README.md
+++ README.md
@@ -1,1 +1,26 @@
-# Planner Daily
+# Daily Planner
+Keep track of your day by adding events to your hours!
+This project was made to showcase ability to implement apis such as dayjs to get hours and days formatted.
+## What was sued to make this
+mostly java script and a handy api called dayjs since starter code was already provided.
+## Link and Photo
+
+
+
+# Table of Contents
+###### 1. Setting Locale Settings for Day.js
+###### 2. Document Ready Event Handler
+###### 3. Handling User Input
+###### 4. Setting Current Time
+###### 5. Setting Colors for Time Blocks
+###### 6. Retrieving and Setting Saved User Input
+###### 7. Setting the Current Date
+###### 8. Putting that Date Into an Html Element
+
+### Credits
+William Tikhonenko
+
+Dayjs API
+
+### Licenses
+MIT
